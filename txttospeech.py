e58369bc1e3deb58248a7ea7c89452bd5d9d851d
Nikhil Padman <nikhilpadman4@gmail.com>
2020-10-25 09:34:59 +0530

    Update txttospeech.py

--- txttospeech.py
+++ txttospeech.py
@@ -1,14 +1,18 @@
+#import required library
 import pyttsx3
-test = "Hi my name is Nikhil Padman!"
+test = "Enter any text here"
+#initializing engine
 engine = pyttsx3.init()
+#initializing var voices
 voices = engine.getProperty('voices')
+#voice_id changes depending on voices preinstalled in system first run above code to list out available voices and its attributes onto screen and then copy desired id as value of voice_id
 #for voice in voices:
  #print("Voice:")
  #print(" - Name: %s" % voice.name)
  #print(" - Languages: %s" % voice.languages)
  #print(" - Gender: %s" % voice.gender)
  #print(" - Age: %s" % voice.age)
-en_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0"
-engine.setProperty('voice', en_voice_id)
+voice_id = #"Enter voice id here"
+engine.setProperty('voice', voice_id)
 engine.say(test)
 engine.runAndWait()
